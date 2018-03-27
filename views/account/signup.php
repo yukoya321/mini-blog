08420f7c2b7dbc5a2842d3712d3d91b7807097a2
yukoya321 <kamo.kamonohashi135@gmail.com>
2018-03-27 23:28:14 +0900

    fix typo

--- views/account/signup.php
+++ views/account/signup.php
@@ -2,7 +2,7 @@
 
 <h2>アカウント登録</h2>
 <form action="<?php echo $base_url;?>/account/register" method="post">
-  <input type="hidden" name="_token" value="<?php echo $this->escape($_token);?>">
+  <input type="hidden" name="_token" value="<?php echo $this->escape($_token);?>" />
   <?php if(isset($errors) && count($erros) > 0): ?>
     <ul class="error_list">
       <?php foreach($errors as $error): ?>
--- views/layout.php
+++ views/layout.php
@@ -7,7 +7,7 @@
 </head>
 <body>
 <div id="header">
-  <h1><a href="<?php echo $base_uri;?>">Mini Blog</a></h1>
+  <h1><a href="<?php echo $base_url;?>">Mini Blog</a></h1>
 </div>
 
 <div id="mail"><?php echo $_content; ?></div>
